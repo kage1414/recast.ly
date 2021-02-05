882e19ed81d0699a06592177184c6c7504546397
Christopher Raffaele <alumnye@gmail.com>
2021-02-04 21:11:13 -0500

    Working on App.jsx

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -4,12 +4,6 @@ import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <ul className="video-list">
-    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
-
     {props.videos.map( (video, idx) =>
       <VideoListEntry key={idx} video={video}/>
     )}
